064c1f76a0d59e95df88576086fe349f9ab40a67
BeatrizUser <beatriz.machadosilva@gmail.com>
2022-06-26 04:34:56 -0300

    melhorias

---
+++ js/script.js
@@ -0,0 +1,297 @@
+(function(){
+	var cnv = document.querySelector("canvas");
+	var ctx = cnv.getContext("2d");
+	
+	var WIDTH = cnv.width, HEIGHT = cnv.height;
+	
+	var LEFT = 37, UP = 38, RIGHT = 39, DOWN = 40;
+	var mvLeft = mvUp = mvRight = mvDown = false;
+	
+	var scene = new Image();
+	var imgPlayer = new Image();
+
+	imgPlayer.src = "img/mario-spriteSimple.png";
+
+	scene.src = "img/smw_tileset.png";
+	scene.addEventListener("load",function(){
+		requestAnimationFrame(loop,cnv);
+	},false);
+
+	
+	var tileSize = 16;
+	var tileSrcSize = 31;
+
+	//array que armazenará os muros do labirinto
+	var walls = [];
+	
+	var player = {
+		x: 100,
+		y: HEIGHT -60,
+		width: 18,
+		height: 31,
+		speed: 2,
+		//atributos de animação
+		srcX: 0,
+		srcY: 0,
+		countAnim: 0
+	};
+	
+var maze = [
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,3,3,3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,3,3,3,3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,5,7,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,4,6,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+    [2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2],
+];
+
+	var T_WIDTH = maze[0].length* tileSize,
+		T_HEIGHT = maze.length * tileSize;
+	
+	//método de preenchimento do array com os muros do labirinto
+	for(var row in maze){
+		for(var column in maze[row]){
+			var tile = maze[row][column];
+			//identificação e criação do objeto muro
+			if(tile > 0){
+				var wall = {
+					x: tileSize*column,
+					y: tileSize*row,
+					width: tileSize,
+					height: tileSize
+				};
+				//inserção no array
+				walls.push(wall);
+			}
+		}
+	}
+
+	var cam = {
+		x: 0,
+		y: 0,
+		width: WIDTH,
+		height: HEIGHT,
+		innerLeftBoundary: function(){
+			return this.x + (this.width*0.25);
+		},
+		innerTopBoundary: function(){
+			return this.y + (this.height*0.25);
+		},
+		innerRightBoundary: function(){
+			return this.x + (this.width*0.75);
+		},
+		innerBottonBoundary: function(){
+			return this.y + (this.height*0.75);
+		},
+	}
+	
+	//função que verifica as colisões e ajusta a posição do personagem bloqueando-o 
+	function blockRectangle(objA,objB){
+		var distX = (objA.x + objA.width/2) - (objB.x + objB.width/2);
+		var distY = (objA.y + objA.height/2) - (objB.y + objB.height/2);
+		
+		var sumWidth = (objA.width + objB.width)/2;
+		var sumHeight = (objA.height + objB.height)/2;
+		
+		if(Math.abs(distX) < sumWidth && Math.abs(distY) < sumHeight){
+			var overlapX = sumWidth - Math.abs(distX);
+			var overlapY = sumHeight - Math.abs(distY);
+			
+			if(overlapX > overlapY){
+				objA.y = distY > 0 ? objA.y + overlapY : objA.y - overlapY;
+			} else {
+				objA.x = distX > 0 ? objA.x + overlapX : objA.x - overlapX;
+			}
+		}
+	}
+	
+	window.addEventListener("keydown",keydownHandler,false);
+	window.addEventListener("keyup",keyupHandler,false);
+	
+	function keydownHandler(e){
+		var key = e.keyCode;
+		switch(key){
+			case LEFT:
+				mvLeft = true;
+				break;
+			case UP:
+				mvUp = true;
+				break;
+			case RIGHT:
+				mvRight = true;
+				break;
+			case DOWN:
+				mvDown = true;
+				break;
+		}
+	}
+	
+	function keyupHandler(e){
+		var key = e.keyCode;
+		switch(key){
+			case LEFT:
+				mvLeft = false;
+				break;
+			case UP:
+				mvUp = false;
+				break;
+			case RIGHT:
+				mvRight = false;
+				break;
+			case DOWN:
+				mvDown = false;
+				break;
+		}
+	}
+	
+	function update(){
+		//animação de movimento
+		if(mvLeft && !mvRight){
+			player.x -= player.speed;
+			player.srcY = player.height;
+		} else 
+		if(mvRight && !mvLeft){
+			player.x += player.speed;
+			player.srcY = 0;
+		}
+		
+		if(mvUp && !mvDown){
+			player.y -= player.speed;
+			player.srcY = tileSrcSize + player.height * 1;
+		} else 
+		if(mvDown && !mvUp){
+			player.y += +50;
+		}else{
+			player.y += player.speed;
+		}
+
+		//processo de animação
+		if(mvLeft || mvRight || mvUp || mvDown){
+			player.countAnim++;
+			
+			if(player.countAnim >= 30){
+				player.countAnim = 0;
+			}
+			                                          //velocidade da animacao
+			player.srcX = Math.floor(player.countAnim/10) * player.width;
+		} else {
+			player.srcX = 0;
+			player.countAnim = 0;
+		}
+
+		
+		//colisao do player com os blocos
+		for(var i in walls){
+			var wall = walls[i];
+			blockRectangle(player,wall);
+		}
+
+		//set da camera
+		if(player.x < cam.innerLeftBoundary()){
+			cam.x = player.x - (cam.width * 0.25)
+		}
+		if(player.y < cam.innerTopBoundary()){
+			cam.y = player.y - (cam.height * 0.25)
+		}
+		if(player.x + player.width > cam.innerRightBoundary()){
+			cam.x = player.x + player.width - (cam.width * 0.75)
+		}
+		if(player.y + player.height > cam.innerBottonBoundary()){
+			cam.y = player.y + player.height - (cam.height * 0.75)
+		}
+
+		cam.x = Math.max(0, Math.min(T_WIDTH - cam.width, cam.x))
+		cam.y = Math.max(0, Math.min(T_HEIGHT - cam.height, cam.y))
+	}
+	
+	function render(){
+		ctx.clearRect(0,0,WIDTH,HEIGHT);
+		ctx.save();
+		ctx.translate(-cam.x, -cam.y)
+		for(var row in maze){
+			for(var column in maze[row]){
+				var tile = maze[row][column];
+
+				var x = column*tileSize;
+				var y = row*tileSize;
+                             // ternario pra orientar localizacao da parede e do chao no sprite
+				
+				//SETAGEM DO CENÁRIO
+				if(tile == 0){
+					var xCoord = 290
+					var yCoord = 0
+				}else
+				if(tile == 1){
+					var xCoord = 34
+					var yCoord = 102
+				}else
+				if(tile == 2){
+					var xCoord = 34
+					var yCoord = 119
+				}else
+				if(tile == 3){
+					var xCoord = 17
+					var yCoord = 0
+				}else
+				if(tile == 4){
+					var xCoord = 85
+					var yCoord = 68
+				}else
+				if(tile == 5){
+					var xCoord = 85
+					var yCoord = 50
+				}else
+				if(tile == 6){
+					var xCoord = 102
+					var yCoord = 68
+				}else
+				if(tile == 7){
+					var xCoord = 102
+					var yCoord = 50
+				}
+
+				ctx.drawImage(
+					scene,
+					xCoord, yCoord, 16, 16,
+					x,y,tileSize,tileSize
+				);
+			}
+		}
+		ctx.drawImage(
+			imgPlayer,
+			player.srcX,player.srcY,player.width,player.height,
+			player.x,player.y,player.width,player.height
+		);
+		ctx.restore();
+	}
+	
+	function loop(){
+		update();
+		render();
+		requestAnimationFrame(loop, cnv);
+	}
+}());
